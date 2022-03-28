d993912edfc446214dfee9e7d3ea35a6eb12d2a3
M.H <hong.ma@tu-ilmenau.de>
2022-03-28 22:08:09 +0200

    change email adress

--- HRTF/audio_with_brir.m
+++ HRTF/audio_with_brir.m
@@ -12,7 +12,7 @@ function out = audio_with_brir(fn,brir)
 %       out: returns stero audio with HRTF in piece of "points_number" 
 %   Author: 	Hong Ma
 %   Created:    Dec 2015
-%   E-mail:     contact(at)mahong.org
+%   E-mail:     ios(at)mahong.me
 %   TU Ilmenau | IMT | Elektronische Medientechnik. 
 %------------------------------------------------------------------------------------------------------------
 
